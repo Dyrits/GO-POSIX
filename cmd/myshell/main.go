c2b14bf6ad00e22d70e2a1f88ca31d51e66b01cd
Dylan J. Gerrits <gerritsdylan@gmail.com>
2024-12-03 20:19:27 +0100

    The echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,25 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+type CommandFunction func(argument string)
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	commands := map[string]func(){
-		"exit 0": func() {
-			os.Exit(0)
+	commands := map[string]CommandFunction{
+		"exit": func(argument string) {
+			if integer, err := strconv.Atoi(argument); err == nil {
+				os.Exit(integer)
+			} else {
+				fmt.Fprint(os.Stdout, "Invalid argument for exit\n")
+			}
+		},
+		"echo": func(argument string) {
+			fmt.Fprint(os.Stdout, argument+"\n")
 		},
 	}
 
 	for {
 		input := read(reader)
-		if function, exists := commands[input]; exists {
-			function()
+		// The shell only works for commands with 4 characters.
+		command := input[0:4]
+		argument := input[5:]
+		if function, exists := commands[command]; exists {
+			function(argument)
 		} else {
 			fmt.Fprint(os.Stdout, fmt.Sprintf("%v: command not found\n", input))
 		}
